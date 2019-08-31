539284749746ebfaf15a12499e08b636fa95210d
pyavchik <pyavchik@gmail.com>
2019-08-31 18:58:10 +0300

    react router setuped

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 const Navbar = () => {
     return (
-        <div>
-
-        </div>
+        <nav className="navbar bg-dark">
+            <h1>
+                <Link to="/"><i className="fas fa-code"></i> EmployeeConnector</Link>
+            </h1>
+            <ul>
+                <li><a href="!#">employee</a></li>
+                <li><Link to="/register">Register</Link></li>
+                <li><Link to="/login">Login</Link></li>
+            </ul>
+        </nav>
     )
 };
 
