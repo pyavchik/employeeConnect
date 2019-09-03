4e45ea6198602050d39f89368a21a61ba4744e28
pyavchik <pyavchik@gmail.com>
2019-09-03 20:46:11 +0300

    typo fixed

--- client/src/components/layout/Alert.js
+++ client/src/components/layout/Alert.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
-const Aler = ({alerts}) =>
+const Alert = ({alerts}) =>
     alerts != null && alerts.length > 0 && alerts.map(alert => (
         <div key={alert.id} className={`alert alert-${alert.alertType}`}>
             {alert.msg}
@@ -17,4 +17,4 @@ const mapStateToProps = state => ({
     alerts: state.alert
 });
 
-export default connect(mapStateToProps)(Aler);
+export default connect(mapStateToProps)(Alert);
